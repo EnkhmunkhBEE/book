295bfe22c4fd089a92d5fa26e1199a41ab6233f0
EnkhmunkhBEE <jingoat1111@gmail.com>
2026-09-14 15:41:38 +0800

    bok

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Header() {
   return (
     <header className="site-header">
-      <Link className="logo" href="/">BOOOK</Link>
+      <Link className="logo" href="/">BOK</Link>
       <nav className="main-nav">
         <Link href="/">Home</Link>
         <Link href="/books">Books</Link>
